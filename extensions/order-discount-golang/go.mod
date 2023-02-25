44ee4369020210c1ab42b62d5b1f96d428b38291
Nick Wesselman <27013789+nickwesselman@users.noreply.github.com>
2023-02-25 12:29:35 -0500

    switch golang to gjson

--- extensions/order-discount-golang/go.mod
+++ extensions/order-discount-golang/go.mod
@@ -2,4 +2,8 @@ module main
 
 go 1.18
 
-require github.com/valyala/fastjson v1.6.4 // indirect
+require (
+	github.com/tidwall/gjson v1.14.4 // indirect
+	github.com/tidwall/match v1.1.1 // indirect
+	github.com/tidwall/pretty v1.2.0 // indirect
+)
--- extensions/order-discount-golang/main/main.go
+++ extensions/order-discount-golang/main/main.go
@@ -3,91 +3,76 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
-	"log"
 	"os"
 
-	"github.com/valyala/fastjson"
+	"github.com/tidwall/gjson"
 )
 
+type Configuration struct {
+	DiscountPercentage     float64
+	QualifyingProductTotal float64
+	QualifyingProductTags  []string
+}
+
+func calculateDiscount(input []byte) string {
+	noDiscount := `{
+		"discountApplicationStrategy": "FIRST",
+		"discounts": []
+	}`
+
+	isVip := gjson.GetBytes(input, "cart.buyerIdentity.customer.metafield.value").Bool()
+	if !isVip {
+		return noDiscount
+	}
+
+	configurationJson := gjson.GetBytes(input, "discountNode.metafield.value").String()
+	configurationParsed := gjson.Parse(configurationJson)
+	configuration := Configuration{
+		DiscountPercentage:     configurationParsed.Get("discountPercentage").Float(),
+		QualifyingProductTotal: configurationParsed.Get("qualifyingProductTotal").Float(),
+	}
+
+	qualifyingTotal := 0.0
+	gjson.GetBytes(input, "cart.lines").ForEach(func(_key gjson.Result, value gjson.Result) bool {
+		isQualifying := value.Get("merchandise.product.isQualifying").Bool()
+		if isQualifying {
+			qualifyingTotal += value.Get("cost.totalAmount.amount").Float()
+		}
+		return true
+	})
+
+	if qualifyingTotal < configuration.QualifyingProductTotal {
+		return noDiscount
+	}
+
+	return fmt.Sprintf(`{
+		"discountApplicationStrategy": "MAXIMUM",
+		"discounts": [
+			{
+				"message": "VIP Discount",
+				"value": {
+					"percentage": {
+						"value": "%.2f"
+					}
+				},
+				"targets": [
+					{
+						"orderSubtotal": {
+							"excludedVariantIds": []
+						}
+					}
+				]
+			}
+		]
+	}`, configuration.DiscountPercentage)
+}
+
 func main() {
-	bytes, err := ioutil.ReadAll(os.Stdin)
+	input, err := ioutil.ReadAll(os.Stdin)
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
 	}
-
-	var p fastjson.Parser
-	value, err := p.ParseBytes(bytes)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	var functionInput = UnmarshalFunctionInput(value)
-	fmt.Println(functionInput.Cart.BuyerIdentity.Customer.Metafield.Value)
-
-	// var input FunctionInput
-	// err = json.Unmarshal(bytes, &input)
-	// if err != nil {
-	// 	fmt.Println(err)
-	// 	os.Exit(1)
-	// }
-
-	// var output api.FunctionResult
-	// output = function(input)
-
-	// bytes, err = json.Marshal(output)
-	// if err != nil {
-	// 	fmt.Println(err)
-	// 	os.Exit(1)
-	// }
-
-	// fmt.Println(string(bytes))
+	discount := calculateDiscount(input)
+	fmt.Println(discount)
 }
-
-// func function(input api.InputResponse) api.FunctionResult {
-// 	var emptyResult = api.FunctionResult{
-// 		Discounts:                   []api.Discount{},
-// 		DiscountApplicationStrategy: api.DiscountApplicationStrategyFirst,
-// 	}
-// 	var buyer = input.Cart.BuyerIdentity
-// 	if buyer == nil {
-// 		return emptyResult
-// 	}
-// 	var customer = buyer.Customer
-// 	if customer == nil {
-// 		return emptyResult
-// 	}
-// 	var vipMetafield = customer.Metafield
-// 	if vipMetafield == nil || vipMetafield.Value != "true" {
-// 		return emptyResult
-// 	}
-
-// 	var message = "VIP Discount"
-// 	var discountPercentage = 0.0
-// 	var configuration = input.DiscountNode.GetConfiguration()
-// 	if configuration != nil {
-// 		discountPercentage = configuration.DiscountPercentage
-// 	}
-
-// 	return api.FunctionResult{
-// 		Discounts: []api.Discount{
-// 			api.Discount{
-// 				Value: api.Value{
-// 					Percentage: &api.Percentage{
-// 						Value: discountPercentage,
-// 					},
-// 				},
-// 				Message: &message,
-// 				Targets: []api.Target{
-// 					api.Target{
-// 						OrderSubtotal: &api.OrderSubtotalTarget{
-// 							ExcludedVariantIds: []string{},
-// 						},
-// 					},
-// 				},
-// 				Conditions: []api.Condition{},
-// 			},
-// 		},
-// 		DiscountApplicationStrategy: api.DiscountApplicationStrategyMaximum,
-// 	}
-// }
