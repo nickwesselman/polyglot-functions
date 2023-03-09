4d9db551a81187c29bb2914198545482cebd178b
Nick Wesselman <27013789+nickwesselman@users.noreply.github.com>
2023-03-09 16:21:31 -0500

    test script tweaks

--- test-all-functions.js
+++ test-all-functions.js
@@ -23,17 +23,30 @@ const runFunction = (path, input) => {
 
 const input = fs.readFileSync('inputs/input.json').toString();
 
+// build a cart with 128 items
+const largeInput = JSON.parse(input);
+for (i = 0; i < 6; i++) {
+    largeInput.cart.lines = largeInput.cart.lines.concat(JSON.parse(JSON.stringify(largeInput.cart.lines)));
+}
+//console.log(JSON.stringify(largeInput, null, 2));
+//console.log(largeInput.cart.lines.length)
+//process.exit(0);
+
 const testData = getFunctionModules()
     .map(module => ({
         module,
-        result: runFunction(module, input)
+        result: runFunction(module, input),
+        largeResult: runFunction(module, JSON.stringify(largeInput))
     }))
     .sort((a, b) => a.result.instructions - b.result.instructions)
-    .map(({ module, result }) => {
+    .map(({ module, result, largeResult }) => {
+        //console.error(result.logs);
+        //console.error(largeResult.logs);
         return {
-            function: module.split(path.sep)[1],
-            "instr (K)": Math.round(result.instructions / 1000),
-            "size (KB)": result.size,
+            "Function": module.split(path.sep)[1],
+            "Kinstr (2 cart lines)": Math.round(result.instructions / 1000),
+            "Kinstr (128 cart lines)": Math.round(largeResult.instructions / 1000),
+            "Size (KB)": result.size,
         }
     });
 
