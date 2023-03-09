cd43b0c7cc9f1866cb6eb5f59aee6f8434ad05b1
Nick Wesselman <27013789+nickwesselman@users.noreply.github.com>
2023-03-09 14:33:57 -0500

    initial work on function test script

---
+++ test-all-functions.js
@@ -0,0 +1,43 @@
+const { execSync } = require('node:child_process');
+const fs = require('fs');
+const path = require('path');
+var toml = require('toml');
+
+const getFunctionModules = () => {
+    return fs.readdirSync('extensions', { withFileTypes: true })
+        .filter(entry => entry.isDirectory())
+        .map(entry => path.join('extensions', entry.name, 'shopify.function.extension.toml'))
+        .filter(tomlPath => fs.existsSync(tomlPath))
+        .map(tomlPath => {
+            const configContent = fs.readFileSync(tomlPath).toString();
+            const config = toml.parse(configContent);
+            return path.join(path.dirname(tomlPath), config.build.path);
+        })
+};
+
+const runFunction = (path, input) => {
+    return JSON.parse(execSync(`npm run function-runner --silent -- -j -f ${path}`, {
+        input
+    }).toString());
+};
+
+const input = fs.readFileSync('inputs/input.json').toString();
+
+const testData = getFunctionModules()
+    .map(module => ({
+        module,
+        result: runFunction(module, input)
+    }))
+    .sort((a, b) => a.result.instructions - b.result.instructions)
+    .map(({module,result}) => {
+        return {
+            function: module.split(path.sep)[1],
+            "instr (K)": Math.round(result.instructions/1000),
+            "size (KB)": result.size,
+        }
+    });
+console.table(testData);
+
+//
+//const result = runFunction('extensions/order-discount-javascript/dist/function.wasm', input);
+//console.log(JSON.stringify(result, null, 2));
