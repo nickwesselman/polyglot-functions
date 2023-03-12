0a01002319e12449da068aed3bad417bc9c667b5
Nick Wesselman <27013789+nickwesselman@users.noreply.github.com>
2023-03-12 10:35:38 -0400

    fix grain build

--- extensions/order-discount-grain/build.sh
+++ extensions/order-discount-grain/build.sh
@@ -1,2 +1,3 @@
+mkdir -p build
 grain compile src/main.gr --no-gc --release --elide-type-info -o build/main-unoptimized.wasm
 wasm-opt -Oz --zero-filled-memory --strip-producers --enable-bulk-memory --enable-multivalue -o build/main.wasm build/main-unoptimized.wasm
