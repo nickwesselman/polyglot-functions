dbf78cd9b35f96380f8f65d965b51e601d1214b1
Nick Wesselman <27013789+nickwesselman@users.noreply.github.com>
2023-03-21 17:08:19 -0400

    tweaks to zig tweaks * restore stdin incremental read * use an allocator for array data

--- app/extensions/order-discount-zig/src/main.zig
+++ app/extensions/order-discount-zig/src/main.zig
@@ -39,11 +39,33 @@ pub fn function(input: api.FunctionInput, allocator: std.mem.Allocator) ?api.Fun
         return noDiscount;
     }
 
-    return api.FunctionResult{ .discounts = &[1]api.Discount{api.Discount{
-        .value = api.Value{ .percentage = api.Percentage{ .value = config.discountPercentage } },
-        .targets = &[1]api.Target{api.Target{ .orderSubtotal = api.OrderSubtotalTarget{ .excludedVariantIds = &[0][]const u8{} } }},
+    var excludedVariantIds = std.ArrayList([]const u8).init(allocator);
+    var targets = std.ArrayList(api.Target).init(allocator);
+    targets.append(.{
+        .orderSubtotal = api.OrderSubtotalTarget {
+            .excludedVariantIds = excludedVariantIds.items
+        }
+    }) catch |err| {
+        std.debug.print("Error allocating target struct: {!}", .{err});
+        return noDiscount;
+    };
+
+    var discounts = std.ArrayList(api.Discount).init(allocator);
+    discounts.append(.{
         .message = "VIP Discount",
-    }}, .discountApplicationStrategy = "FIRST" };
+        .value = api.Value {
+            .percentage = api.Percentage{ .value = config.discountPercentage }
+        },
+        .targets = targets.items,
+    }) catch |err| {
+        std.debug.print("Error allocating discount struct: {!}", .{err});
+        return noDiscount;
+    };
+
+    return api.FunctionResult {
+        .discounts = discounts.items,
+        .discountApplicationStrategy = "FIRST"
+    };
 }
 
 pub fn main() u8 {
@@ -52,13 +74,25 @@ pub fn main() u8 {
 
     const allocator = arena.allocator();
 
-    var buffer: [3 * 1024]u8 = undefined;
-    const n = std.io.getStdIn().readAll(&buffer) catch |err| {
-        std.debug.print("Error reading function input: {!}", .{err});
-        return 1;
-    };
+    // Read all stdin to an ArrayList
+    var inputStr = std.ArrayList(u8).init(allocator);
+    var stdin = std.io.getStdIn().reader();
+    var buffer: [1024]u8 = undefined;
+    while (true) {
+        const bytesRead = stdin.read(&buffer) catch |err| {
+            std.debug.print("Error reading function input: {!}", .{err});
+            return 1;
+        };
+        if (bytesRead == 0) {
+            break; // No more data
+        }
+        inputStr.appendSlice(buffer[0..bytesRead]) catch |err| {
+            std.debug.print("Error reading function input: {!}", .{err});
+            return 1;
+        };
+    }
 
-    var stream = std.json.TokenStream.init(buffer[0..n]);
+    var stream = std.json.TokenStream.init(inputStr.items);
     const input = std.json.parse(api.FunctionInput, &stream, .{ .allocator = allocator }) catch |err| {
         std.debug.print("Error parsing function input: {}\n", .{err});
         return 1;
