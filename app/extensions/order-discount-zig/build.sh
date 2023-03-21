e8a16f2e5e522e78816fdba308d1d0d29a433f55
Surma <surma@shopify.com>
2023-03-21 12:28:04 +0000

    Slight memcpy optimizaton in Zig

--- app/extensions/order-discount-zig/build.sh
+++ app/extensions/order-discount-zig/build.sh
@@ -1,2 +1,4 @@
-zig build -Drelease-small=true
-wasm-opt -Oz --zero-filled-memory --strip-producers --enable-bulk-memory -o zig-out/bin/order-discount-zig-optimized.wasm zig-out/bin/order-discount-zig.wasm
+zig build -Drelease-fast=true
+# There is an invalid DWARF header in the binary which makes wasm-opt fail. Stripping removes all DWARF stuff.
+wasm-strip zig-out/bin/order-discount-zig.wasm
+wasm-opt -O3 --zero-filled-memory --enable-bulk-memory -o zig-out/bin/order-discount-zig-optimized.wasm zig-out/bin/order-discount-zig.wasm
--- app/extensions/order-discount-zig/src/main.zig
+++ app/extensions/order-discount-zig/src/main.zig
@@ -1,24 +1,18 @@
 const std = @import("std");
 const api = @import("./api.zig");
 
-const Configuration = struct {
-    discountPercentage: f64 = 0,
-    qualifyingProductTotal: f64 = 0
-};
+const Configuration = struct { discountPercentage: f64 = 0, qualifyingProductTotal: f64 = 0 };
 
 pub fn function(input: api.FunctionInput, allocator: std.mem.Allocator) ?api.FunctionResult {
-    const noDiscount = api.FunctionResult {
-        .discounts = ([0]api.Discount {})[0..],
-        .discountApplicationStrategy = "MAXIMUM"
-    };
+    const noDiscount = api.FunctionResult{ .discounts = &[0]api.Discount{}, .discountApplicationStrategy = "MAXIMUM" };
 
     var isVip: bool = false;
     if (input.cart.buyerIdentity) |buyer| if (buyer.customer) |customer|
-        if (customer.metafield) |metafield| if (metafield.value) |valueStr| 
+        if (customer.metafield) |metafield| if (metafield.value) |valueStr|
             if (std.mem.eql(u8, valueStr, "true")) {
                 isVip = true;
             };
-    
+
     if (!isVip) {
         return noDiscount;
     }
@@ -26,11 +20,8 @@ pub fn function(input: api.FunctionInput, allocator: std.mem.Allocator) ?api.Fun
     var config: Configuration = .{};
     if (input.discountNode.metafield) |metafield| if (metafield.value) |configStr| {
         var stream = std.json.TokenStream.init(configStr);
-        config = std.json.parse(Configuration, &stream, .{
-            .allocator = allocator,
-            .ignore_unknown_fields = true
-        }) catch |err| {
-            std.debug.print("Error parsing function config: {!}",.{err});
+        config = std.json.parse(Configuration, &stream, .{ .allocator = allocator, .ignore_unknown_fields = true }) catch |err| {
+            std.debug.print("Error parsing function config: {!}", .{err});
             return noDiscount;
         };
     };
@@ -48,26 +39,11 @@ pub fn function(input: api.FunctionInput, allocator: std.mem.Allocator) ?api.Fun
         return noDiscount;
     }
 
-    return api.FunctionResult {
-        .discounts = ([1]api.Discount {
-            api.Discount {
-                .value = api.Value {
-                    .percentage = api.Percentage {
-                        .value = config.discountPercentage
-                    }
-                },
-                .targets = ([1]api.Target {
-                    api.Target {
-                        .orderSubtotal = api.OrderSubtotalTarget {
-                            .excludedVariantIds = ([0][]const u8 {})[0..]
-                        }
-                    }
-                })[0..],
-                .message = "VIP Discount",
-            }
-        })[0..],
-        .discountApplicationStrategy = "FIRST"
-    };
+    return api.FunctionResult{ .discounts = &[1]api.Discount{api.Discount{
+        .value = api.Value{ .percentage = api.Percentage{ .value = config.discountPercentage } },
+        .targets = &[1]api.Target{api.Target{ .orderSubtotal = api.OrderSubtotalTarget{ .excludedVariantIds = &[0][]const u8{} } }},
+        .message = "VIP Discount",
+    }}, .discountApplicationStrategy = "FIRST" };
 }
 
 pub fn main() u8 {
@@ -76,36 +52,22 @@ pub fn main() u8 {
 
     const allocator = arena.allocator();
 
-    // Read all stdin to an ArrayList
-    var inputStr = std.ArrayList(u8).init(allocator);
-    var stdin = std.io.getStdIn().reader();
-    var buffer: [1024]u8 = undefined;
-    while (true) {
-        const bytesRead = stdin.read(&buffer) catch |err| {
-            std.debug.print("Error reading function input: {!}",.{err});
-            return 1;
-        };
-        if (bytesRead == 0) {
-            break; // No more data
-        }
-        inputStr.appendSlice(buffer[0..bytesRead]) catch |err| {
-            std.debug.print("Error reading function input: {!}",.{err});
-            return 1;
-        };
-    }
-
-    var stream = std.json.TokenStream.init(inputStr.items);
-    const input = std.json.parse(api.FunctionInput, &stream, .{
-        .allocator = allocator
-    }) catch |err| {
-        std.debug.print("Error parsing function input: {!}",.{err});
+    var buffer: [3 * 1024]u8 = undefined;
+    const n = std.io.getStdIn().readAll(&buffer) catch |err| {
+        std.debug.print("Error reading function input: {!}", .{err});
         return 1;
     };
-    
+
+    var stream = std.json.TokenStream.init(buffer[0..n]);
+    const input = std.json.parse(api.FunctionInput, &stream, .{ .allocator = allocator }) catch |err| {
+        std.debug.print("Error parsing function input: {}\n", .{err});
+        return 1;
+    };
+
     const result = function(input, allocator);
 
     std.json.stringify(result, .{}, std.io.getStdOut().writer()) catch |err| {
-        std.debug.print("Error serializing function output: {!}",.{err});
+        std.debug.print("Error serializing function output: {!}", .{err});
         return 1;
     };
 
