754a91998dc90df03a3caa45917a003712366032
Surma <surma@shopify.com>
2023-03-21 12:33:10 +0000

    Allow folks to overwrite the WASI_SDK_PATH

--- app/extensions/order-discount-cpp/build.sh
+++ app/extensions/order-discount-cpp/build.sh
@@ -1,5 +1,5 @@
 export WASI_VERSION_FULL=19.0
-export WASI_SDK_PATH=/usr/local/wasi-sdk-${WASI_VERSION_FULL}
+export WASI_SDK_PATH=${WASI_SDK_PATH:-/usr/local/wasi-sdk-${WASI_VERSION_FULL}}
 CC="${WASI_SDK_PATH}/bin/clang++ -Oz -fno-exceptions -std=c++17 --sysroot=${WASI_SDK_PATH}/share/wasi-sysroot"
 
 mkdir -p build
